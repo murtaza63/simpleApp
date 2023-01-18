938e80e65610402969c79ac677ec7b657bde721e
Ghulam Murtaza <murtaza63@gmail.com>
2023-01-18 16:13:00 +0500

    Initial Commit

---
+++ simpleApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  simpleApp
+//
+//  Created by Ghulam Murtaza on 18/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleApp/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  simpleApp
+//
+//  Created by Ghulam Murtaza on 18/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
